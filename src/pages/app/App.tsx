8fec5dff3803032c229c5a62aaa67b0fd63913b6
Gene Hall <genehall@eworldes.com>
2023-05-16 16:24:34 -1000

    remove cra stuff

---
+++ src/pages/app/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          TaskBoard
+      </header>
+    </div>
+  );
+}
+
+export default App;
