ca2d93c71b929f06db9fcd46432d1af2302969ae
Gene Hall <genehall@eworldes.com>
2023-05-23 15:34:25 -1000

    mock up header

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,5 +1,13 @@
+import "./AppHeader.scss";
+
 const AppHeader = () => {
-  return <div className="app-header">This is the header</div>;
+  return (
+    <div className="app-header">
+      <h1 className="title">Task Board</h1>
+      <input type="text" className="search" placeholder="Search" />
+      <div className="right">username</div>
+    </div>
+  );
 };
 
 export default AppHeader;
